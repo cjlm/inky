c8f8fb12f257f23bd49e6a03a21833d3b16821c6
cjlm <christian.miles@gmail.com>
2022-01-29 20:10:19 -0800

    add calling script, put meme in function

---
+++ setInky.py
@@ -0,0 +1,6 @@
+import random
+
+from setMeme import setMeme
+from setHighlight import setHighlight
+
+random.choice([setMeme, setHighlight])()
--- setMeme.py
+++ setMeme.py
@@ -12,35 +12,36 @@ from selenium.webdriver.support.wait import WebDriverWait
     
 
 from selenium.webdriver.chrome.options import Options
-options = Options()
-options.add_argument("--headless")
 
-browser = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver',options=options);
+def setMeme():
+    options = Options()
+    options.add_argument("--headless")
 
-browser.set_window_size(1920, 2000)
+    browser = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver',options=options);
 
-browser.get("https://imgflip.com/ai-meme") 
+    browser.set_window_size(1920, 2000)
 
-def wait():
-    WebDriverWait(browser, 10000).until(EC.invisibility_of_element_located((By.ID, "site-loading")))    
+    browser.get("https://imgflip.com/ai-meme") 
 
-wait()
-randomButtonEl = browser.find_element_by_id("aim-random-btn")
-randomButtonEl.click()
-wait()
+    def wait():
+        WebDriverWait(browser, 10000).until(EC.invisibility_of_element_located((By.ID, "site-loading")))    
 
-featureElement = browser.find_element_by_xpath("//*[@id='aim-preview-wrap']") 
-location = featureElement.location 
-size = featureElement.size 
-browser.save_screenshot("fullPageScreenshot.png") 
-x = location['x'] 
-y = location['y'] 
-w = x + size['width'] 
-h = y + size['height'] - 80
-fullImg = Image.open("fullPageScreenshot.png") 
-cropImg = fullImg.crop((x, y, w, h)) 
-cropImg.save('croppedMeme.png') 
-browser.quit()  
+    wait()
+    randomButtonEl = browser.find_element_by_id("aim-random-btn")
+    randomButtonEl.click()
+    wait()
 
-convertSetImage('croppedMeme.png')
+    featureElement = browser.find_element_by_xpath("//*[@id='aim-preview-wrap']") 
+    location = featureElement.location 
+    size = featureElement.size 
+    browser.save_screenshot("fullPageScreenshot.png") 
+    x = location['x'] 
+    y = location['y'] 
+    w = x + size['width'] 
+    h = y + size['height'] - 80
+    fullImg = Image.open("fullPageScreenshot.png") 
+    cropImg = fullImg.crop((x, y, w, h)) 
+    cropImg.save('croppedMeme.png') 
+    browser.quit()  
 
+    convertSetImage('croppedMeme.png')
